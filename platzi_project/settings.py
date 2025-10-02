f04241926e2f8a18a706a0e9c9926f9dfed494e4
Julian <juliandavidmogollon0803@gmail.com>
2025-10-02 11:19:22 -0500

    cambie nombre de las carpetas templates

--- platzi_project/settings.py
+++ platzi_project/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'platzi_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'Templates'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
